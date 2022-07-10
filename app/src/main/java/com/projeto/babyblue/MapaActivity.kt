32bbd6d7e4b3c19d92b69591f65bdfb7aa10c218
Floriano Bauer <40305451+fn-bauer@users.noreply.github.com>
2022-07-10 12:08:01 -0400

    Código com Erro Compat V7

---
+++ app/src/main/java/com/projeto/babyblue/MapaActivity.kt
@@ -0,0 +1,11 @@
+package com.projeto.babyblue
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MapaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapa)
+    }
+}
